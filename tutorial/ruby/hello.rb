fb5fdc374a00321595a10ab48d1a924dca849e75
taketake <summer.boy.japan@gmial.com>
2019-11-01 20:12:41 +0900

    ここまでならわかった。

--- tutorial/ruby/hello.rb
+++ tutorial/ruby/hello.rb
@@ -1,3 +1,118 @@
-print "hello world"
-puts "hello"
-p "hey"
+
+# scores = {taguchi:200,fkoji:400}
+# p scores[:taguchi]
+# scores[:fkoji]=432
+# p scores
+
+# p scores.size
+# p scores.keys
+# p scores.values
+
+# puts "hello"
+# puts %Q(helllo""""")
+# p ["red","blue"]
+# p %W(red blue)
+
+# p "name: %s" % "taguchi"
+# p "name: %-10s" % "tagushi"
+# p "name: %+10s" % "tagushi"
+
+# p "id: %d" % 355
+
+# p "id: %04d, rate: %10.4f" % [354,2.3412]
+
+# printf("name: %10s" , "okada\n")
+# p sprintf("id:%05d, rate:%-10.2f\n", 234,234.34343343)
+
+# score = gets.to_i
+
+# if score>80
+#     puts "great"
+# elsif score>60
+#     puts "good" 
+# else
+#     puts "soso...."
+# end
+
+# puts "great" if score>80
+
+# signal= gets.chomp
+
+# case signal
+# when "red" 
+#     puts "stop"
+# else 
+#     puts "wrong signal"
+# end        
+
+# i=0
+
+# while i<10 do
+#     puts "#{i}:helllo"
+#     i+=1
+# end
+
+# 10.times { |i|
+#     puts "#{i}:lfkdjs"
+# }
+
+# for i in 15..20 
+#     p i
+# end
+
+# for color in["red","blue"] do
+#     p color
+# end
+
+# {taniguchi:200, fkoji:4000}.each { |name,score|
+#     puts "#{name}:#{score}"
+# }
+
+# i=0
+# # loop do 
+# #     p i
+# #     i += 1
+# 10.times do |i|
+#     if i == 7 then
+#         next
+#     end
+#     p i
+# end
+
+# def sayHi(name ="tom") #hikisuu
+#     # puts"hi! #{name}"
+#     return puts "hi! #{name}"
+# end
+
+# sayHi("taguchi")
+
+#Class
+
+class User
+
+    def initialize(name)
+        @name = name
+    end
+
+    def sayHi
+        puts "hi! i am #{@name}" 
+    end
+end
+
+tom = User.new("tom")
+tom.sayHi
+
+
+class AdminUser < User
+    #User 親クラス
+    #AdminUser こくらす
+
+    def sayHello
+        puts "Hello from #{@name}"
+    end
+
+end
+
+tom = AdminUser.new("tom")
+tom.sayHi
+tom.sayHello
